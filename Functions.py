cc21c6e91e9b7fc70f34e786bd712202f9114289
hjt523 <harry.tindale1997@gmail.com>
2021-05-04 12:01:17 +0100

    Added function that finds out if one string can be made by removing a character from another

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 #This file contains the functions practice material, commented out.
+#Task: Write code to add numbers using functions
 '''
 def add_calc(number1, number2):
     answer = number1 + number2
@@ -8,19 +9,20 @@ added_number = add_calc(5,5)
 
 print(added_number + 20)
 '''
-
+#Task: write code to calc grade percent and letter using functions
+'''
 H = float(input("your homework score : " ))
 ass = float(input("your assessment score : "))
 final = float(input("your final exam score : " ))
 
 def letter(perc):
-    if(perc >= 90):
+    if(perc >= 70):
         return( 'A' )
-    elif( 90 > perc >=80):
+    elif( 70 > perc >=60):
         return( 'B' )
-    elif( 80 > perc >= 70):
+    elif( 60 > perc >= 50):
         return ( 'C')
-    elif(70 > perc >= 60):
+    elif(50> perc >= 40):
         return ('D')
     else :
         return ('F')
@@ -35,6 +37,30 @@ def grades(H,ass,final):
     return H,hletter, ass,assletter, final,finalletter
 
 print(f"Homework, assessment and final exam Percentages : {grades(H,ass,final)}")
+'''
+#Task Near - Additional tasks
 
+#Create a fn that when given two strings of letters, determines whether the 2nd can be made from the first by removing one letter
+'''
+def near(a,b):
+    trial=""
+    yasss = False
+    for j in range(0, len(a)):
+        for i in range(0,len(a)):
+            if i != j:
+                trial= trial + a[i]
+        if (trial == b):
+            yasss = True
+        trial =""
+    return yasss
+
+print("Find out if removing a letter from string1 makes string2!")
+print( "String1 should be larger than string 2 for this to work")
+a = input("string1 : ")
+b = input("string2 : ")
+
+print(near(a,b))
+
+'''
 
 
